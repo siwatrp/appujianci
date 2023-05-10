4aba8febe1983599ea98615fc9ea339a0e7d3af7
siwatrp <asiwanuraizzati@gmail.com>
2023-05-10 14:48:38 +0700

    menambah komentar

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -23,6 +23,7 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
+        // ini komentar
         $blade = new Blade(VIEWPATH, APPPATH . 'cache');
         echo $blade->make('form', [])->render();
     }
